0e9c47ee51939e9da24dfc8d8b1490ffb12c1b0a
Alon Grinberg Dana <alon@technion.ac.il>
2025-04-28 14:37:59 +0300

    tmp

--- src/schema.py
+++ src/schema.py
@@ -22,31 +22,24 @@ class TerminationTimeEnum(str, Enum):
     hours = 'hours'
     days = 'days'
 
-class BEESCompounds(BaseModel):
+
+class Species(BaseModel):
     """
-    A class for validating input.BEES.compound arguments
+    A class for validating input.BEES.species arguments
     """
     label: str
-    type: Literal["substrate", "enzyme", "cofactor"]
     concentration: Union[confloat(gt=0), Tuple[confloat(gt=0), confloat(gt=0)]] = None # concentration in mol/L
-    structure_smiles: Optional[str] = None
-    structure_inchi: Optional[str] = None 
-    charge: Optional[float] = None
-    compartment: Optional[str] = None  # e.g., 'cytoplasm', 
-    kinetic_params: Optional[Dict[str, float]] = None # e.g., {"Km": 0.1, "Vmax": 2.0}
-    reactive: bool = True
+    smiles: Optional[str] = None
+    inchi: Optional[str] = None
+    charge: Optional[float] = 0
     constant: bool = False
-    observable: bool = False # True if the compound is observable, meaning it can be measured and tracked in the simulation
-    SA_observable: bool = False # True if the compound is SA observable, meaning it can be measured and tracked in the sanstivity analysis simulation
-    UA_observable: bool = False #  True if the compound is UA observable, meaning it can be measured and tracked in the uncertainty analysis simulation
+    reactive: bool = True
+    observable: bool = False  # Whether this is an observable for SA
     solvent: bool = False # True if the compound is a solvent
-    xyz: Optional[Union[List[Union[dict, str]], dict, str]] = None # Holds 3D coordinates (geometry)
-    #seed_all_rads: Optional[List[RadicalTypeEnum]] = None
-    
-    
+    xyz: Optional[Union[dict, str]] = None
+
     class Config:
         extra = "forbid"
-
 
     @field_validator('concentration')
     def check_concentration_range_order(cls, value, values):
@@ -74,19 +67,101 @@ class BEESCompounds(BaseModel):
             raise ValueError("Constant species cannot have a concentration range.")
         return value
     
-    #@field_validator('reactive') 
-    #def check_reactive(cls, value, values):
-        #"""Check if the compound is reactive"""
-        #if value and values.get('constant'):
-            #raise ValueError("Reactive species cannot be constant.")
-        #return value
+    @field_validator('reactive')
+    def check_reactive(cls, value, values):
+        """Check if the compound is reactive"""
+        if value and values.get('constant'):
+            raise ValueError("Reactive species cannot be constant.")
+        return value
 
-    #@field_validator('observable')       
-    #def check_observable(cls, value, values):  
-        #"""Check if the compound is observable"""
-        #if value and values.get('constant'):
-            #raise ValueError("Observable species cannot be constant.")
-        #return value
+    @field_validator('observable')
+    def check_observable(cls, value, values):
+        """Check if the compound is observable"""
+        if value and values.get('constant'):
+            raise ValueError("Observable species cannot be constant.")
+        return value
+
+    @field_validator('structure_smiles')
+    def check_structure_smiles(cls, value):
+        """Check if the structure is valid"""
+        if value and not isinstance(value, str):
+            raise ValueError("Structure must be a string.")
+        if Chem.MolFromSmiles(value) is None:
+            raise ValueError(f"Invalid SMILES string: {value}")
+        return value
+
+    @field_validator('structure_inchi')
+    def validate_inchi(cls, value):
+        """Check that InChI string is chemically valid"""
+        if value is None:
+            return value
+        try:
+            from rdkit.Chem import inchi
+            mol = inchi.MolFromInchi(value)
+            if mol is None:
+                raise ValueError
+        except Exception:
+            raise ValueError(f"Invalid InChI string: {value}")
+        return value
+
+
+class Enzyme(BaseModel): # TODO: add enzyme specific fields
+    """
+    A class for validating input.BEES.species arguments
+    """
+    label: str
+    concentration: Union[confloat(gt=0), Tuple[confloat(gt=0), confloat(gt=0)]] = None # concentration in mol/L
+    smiles: Optional[str] = None
+    inchi: Optional[str] = None
+    charge: Optional[float] = 0
+    constant: bool = False
+    reactive: bool = True
+    observable: bool = False  # Whether this is an observable for SA
+    solvent: bool = False # True if the compound is a solvent
+    xyz: Optional[Union[dict, str]] = None
+
+    class Config:
+        extra = "forbid"
+
+    @field_validator('concentration')
+    def check_concentration_range_order(cls, value, values):
+        """Ensure range is valid and ordered"""
+        label = values.get('label')
+        if value is None:
+            raise ValueError(f"Concentration must be specified for '{label}'")
+        if isinstance(value, float):
+            if value < 0:
+                raise ValueError(f"Concentration cannot be negative. Got {value} for '{label}'")
+            return value
+        # If it's a tuple, check the range
+        if isinstance(value, tuple):
+            if value[0] == value[1]:
+                raise ValueError(f"Concentration range cannot have identical values. Got {value} for '{label}'")
+            if value[0] > value[1]:
+                value = (value[1], value[0])
+            if value[0] < 0 or value[1] < 0:
+                raise ValueError(f"Concentration cannot be negative")
+        return value
+
+    @field_validator('constant')
+    def check_constant_species(cls, value, values):
+        if value and isinstance(values.get('concentration'), tuple):
+            raise ValueError("Constant species cannot have a concentration range.")
+        return value
+
+    @field_validator('reactive')
+    def check_reactive(cls, value, values):
+        """Check if the compound is reactive"""
+        if value and values.get('constant'):
+            raise ValueError("Reactive species cannot be constant.")
+        return value
+
+    @field_validator('observable')
+    def check_observable(cls, value, values):
+        """Check if the compound is observable"""
+        if value and values.get('constant'):
+            raise ValueError("Observable species cannot be constant.")
+        return value
 
     @field_validator('structure_smiles')
     def check_structure_smiles(cls, value):
@@ -141,7 +216,7 @@ class BEESSpeciesConstraints(BaseModel):
                                  f"Got: {val} in {value}")
         return value
 
-class BEESReactionRule(BaseModel):
+class Database(BaseModel):
     name: str
     thermo_libraries: Optional[List[str]] = None
     kinetics_libraries: Optional[List[str]] = None
@@ -168,14 +243,11 @@ class BEESEnvironment(BaseModel):
     """
     A class for validating input.BEES.Enviornment arguments
     """
-    
-    temperature: Union[confloat(gt=0), List[confloat(gt=0)]]  # in Celsius or Kelvin ?
+    temperature: Union[confloat(gt=0), List[confloat(gt=0)]]  # in Celsius
     pH: Union[confloat(gt=0), List[confloat(gt=0)]]
-   #Volume: Optional[Union[confloat(gt=0), List[confloat(gt=0)]]] = None
     ionic_strength: Optional[float] = None
-    Oxygen_level: Optional[float] = None
-    seed_mechanisms: List[str] = list()
-
+    oxygen_level: Optional[float] = None
+    seed_mechanisms: List[str] = list()   # todo: database
 
     @field_validator('temperature')
     def check_t(cls, value):
@@ -198,24 +270,15 @@ class BEESEnvironment(BaseModel):
             raise ValueError(f'Ionic strength cannot be negative. Got: {value}')
         return value
 
-    @field_validator('Oxygen_level')
+    @field_validator('oxygen_level')
     def validate_oxygen_level(cls, value):
-        """BEESEnvironment.Oxgen_level field_validator"""
+        """BEESEnvironment.oxgen_level field_validator"""
         if value is not None and not (0 <= value <= 1):
             raise ValueError(f'Oxygen level must be between 0 and 1. Got: {value}')
         return value
     
-    #@field_validator('V', always=True)
-    #def check_v(cls, value, values):
-        """BEESEnvironment.volume field_validator"""
-        if isinstance(value, list) and len(value) != 2:
-            raise ValueError(f'When specifying the volume as a list, only two values are allowed (V min, V max),\n'
-                             f'got {len(value)} values: {value}.')
-        if 'type' in values and 'liquid' in values['type'] and value is None:
-            raise ValueError('The reactor volume must be specified for a liquid-phase reactor.')
     
-    
-class BEESModelSettings(BaseModel):
+class Settings(BaseModel):
     end_time: confloat(gt=0)  
     time_step: confloat(gt=0)
     solver: Literal["odeint", "CVODE", "custom"]
@@ -253,67 +316,13 @@ class BEESInputBase(BaseModel):
     """
     An InputBase class for validating input arguments
     """
-    BEES_execution_type: Optional[str] = None
-    memory: Optional[conint(ge=0)] = None
-    cpus: Optional[conint(gt=0)] = None
     project: constr(max_length=255)
     project_directory: Optional[constr(max_length=255)] = None
-    compounds: List[BEESCompounds]
+    species: List[Species]
+    enzymes: List[Enzyme]
     environment: BEESEnvironment
-    rules: List[BEESReactionRule]
-    model_settings: BEESModelSettings
-
-    #verbose: conint(ge=10, le=30, multiple_of=10) = 20     #didn't get what this line doing 
-    
-    #qm: Optional[QM] = None
+    database: Database
+    settings: Settings
 
     class Config:
         extra = "forbid"
-
-    #@field_validator('t3', always=True)
-    #def check_t3(cls, value):
-        #"""InputBase.t3 field_validator"""
-        #return value or T3()
-
-    #@field_validator('qm', always=True)
-    #def check_qm(cls, value):
-        """InputBase.qm field_validator"""
-        # Removed invalid return statement
-        pass
-
-    #@root_field_validator(pre=True)
-    #def validate_rmg_t3(cls, values):
-       # """InputBase.validate_rmg_t3"""
-        #if 'rmg' in values and 't3' in values and values['t3']:
-            # check termination time for global UA
-         #   if 'uncertainty' in values['t3'] and values['t3']['uncertainty'] is not None:
-          #      ua_termination_time = values['t3']['uncertainty']['termination_time']
-           #     rmg_reactor_termination_times = [reactor['termination_time'] for reactor in values['rmg']['reactors']]
-            #    if all([termination_time is None for termination_time in rmg_reactor_termination_times]) \
-             #           and ua_termination_time is None and values['t3']['uncertainty']['global_analysis']:
-              #      raise ValueError('If a global uncertainty analysis is requested, a termination time must be '
-               #                      'specified either under "t3.uncertainty.termination_time" '
-                #                     'or in at least one RMG reactor.')
-            # check solvent for liquid phase
-            #reactor_types = set([reactor['type'] for reactor in values['rmg']['reactors']])
-            #solvents = list()
-            #for species in values['rmg']['species']:
-                #if 'solvent' in species and species['solvent']:
-                    #solvents.append(species['label'])
-            #if any(['liquid' in reactor_type for reactor_type in reactor_types]):
-                #if not len(solvents):
-                    #raise ValueError('One species must be defined as the solvent when using liquid phase reactors.')
-                #if len(solvents) > 1:
-                    #raise ValueError(f'Only one solvent can be specified, got: {solvents}')
-            #else:
-                #if len(solvents):
-                 #   raise ValueError(f'No solvent species are allowed for gas phase reactors, got: {solvents}')
-            # check core_tolerance and max_T3_iterations
-            #if 'model' in values['rmg'] and 'core_tolerance' in values['rmg']['model'] \
-             #       and 'options' in values['t3'] and 'max_T3_iterations' in values['t3']['options'] \
-              #      and not isinstance(values['rmg']['model']['core_tolerance'], float) \
-               #     and len(values['rmg']['model']['core_tolerance']) > values['t3']['options']['max_T3_iterations']:
-                #raise ValueError(f'The number of RMG core tolerances ({len(values["rmg"]["model"]["core_tolerance"])}) '
-                 #                f'cannot be greater than the max number of T3 iterations '
-                   #              f'({values["t3"]["options"]["max_T3_iterations"]}).')
-        #return values
